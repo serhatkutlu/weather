4df57b533585ee116e7cd543fd825d48ec468aae
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-02-16 12:52:44 +0300

    resource created

---
+++ app/src/main/java/com/msk/weather/Util/Resource.kt
@@ -0,0 +1,8 @@
+package com.msk.weather.Util
+
+
+sealed class Resource<T>(val data :T? =null,val message:String?=null){
+    class Success <T>(data : T) : Resource<T>(data)
+    class Error <T>(message: String?=null,data : T?=null) : Resource<T>(data,message )
+    class Loading<T>():Resource<T>()
+}
